9204d017de1985ac922cde1034363168925c2446
Lee Pedersen <leepedersenr@gmail.com>
2022-01-06 12:22:40 -0700

    fix font styling, create onclick sculpture method

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,14 @@ import '../css/header.css';
 import { SiInstagram } from "react-icons/si";
 
 function Header(props) {
+  function handleSculptureClick() {
+    console.log("clicked");
+  }
   return(
     <div className="leftColumn">
       <h1>Hardy Izard</h1>
       <div className="links">
-      <p onClick={props.handleSculptureClick}>Sculpture</p>
+      <p onClick={handleSculptureClick}>Sculpture</p>
       <p>Videos</p>
       <p onClick={props.handleAboutClick}>About</p>
       <SiInstagram className="instagram"/>
