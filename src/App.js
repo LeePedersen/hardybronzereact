f4aa95d5ebc1ea23d4bb8eceeb9ca11c47058d63
Lee Pedersen <leepedersenr@gmail.com>
2022-01-13 16:37:28 -0700

    switch from menu component to header component based on screen size

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/App.css';
+import Header from './components/header';
+import Menu from './components/menu';
 import Sculpture from './components/sculpture';
 import About from './components/about';
 import Video from './components/video';
 import Contact from './components/contact';
-import Header from './components/header';
 import ViewImage from './components/viewImage';
 import Error404 from './components/Error404';
 import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="background">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Sculpture} />
-        <Route path='/about' component={About} />
-        <Route path='/video' component={Video} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/viewImage' component={ViewImage} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      useMenu: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
+  }
+
+  resize() {
+    const currentUseMenu = (window.innerWidth <= 768);
+    const currentUseHeader = (window.innerWidth >= 768);
+    if (currentUseMenu !== this.state.useMenu) {
+      this.setState({useMenu: currentUseMenu});
+    }
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener("resize", this.resize.bind(this));
+  }
+
+  render() {
+    console.log(this.state.useMenu);
+    return (
+      <div className="background">
+        {this.state.useMenu ? <Menu /> : <Header />}
+        <Switch>
+          <Route exact path='/' component={Sculpture} />
+          <Route path='/about' component={About} />
+          <Route path='/video' component={Video} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/viewImage' component={ViewImage} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+
+  }
 }
 
 export default App;
--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from 'react-router-dom';
 import '../css/header.css';
 import { SiInstagram } from "react-icons/si";
