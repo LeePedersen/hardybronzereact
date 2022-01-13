e415abc74d80e453a922bf43713b0e15dce71b3d
Lee Pedersen <leepedersenr@gmail.com>
2022-01-12 22:16:35 -0700

    start mobile responsive styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import '../css/header.css';
 import { SiInstagram } from "react-icons/si";
 
 function Header() {
+  // componentDidMount() {
+  //   window.addEventListener("resize", this.resize.bind(this));
+  //   this.resize();
+  // }
+  //
+  // resize() {
+  //   let currentHideNav = (window.innerWidth <= 760);
+  //   if (currentHideNav !== this.state.hideNav) {
+  //     this.setState({hideNav: currentHideNav});
+  //   }
+  // }
+  //
+  // componentWillUnmount() {
+  //     window.removeEventListener("resize", this.resize.bind(this));
+  // }
+  //
+  // const [shown, setShown] = useState(false);
+  // const showMenu = () => {
+  //   setShown(true);
+  // }
+
   return (
     <div className="leftColumn">
-      <h1>Hardy Izard</h1>
+      <h1 className="nameHeader">Hardy Izard</h1>
       <p><Link to="/">Sculpture</Link></p>
       <p><Link to="About">About</Link></p>
       <p><Link to="Video">Videos</Link></p>
