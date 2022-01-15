ae1332cc193cb121964147f344cc4b5c9d498421
Lee Pedersen <leepedersenr@gmail.com>
2022-01-15 13:54:44 -0700

    make menu toggle when clicked

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,23 +1,77 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import '../css/header.css';
+import '../css/menu.css';
 import { SiInstagram } from "react-icons/si";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import { FiMenu } from "react-icons/fi";
 
-function Menu() {
+class Menu extends React.Component {
 
-  return (
-    <div className="menu">
-      <h1 className="nameHeader">Hardy Izard</h1>
-      <Link to="/">Sculpture</Link>
-      <Link to="About">About</Link>
-      <Link to="Video">Videos</Link>
-      <Link to="Contact">Contact</Link>
-      <SiInstagram className="instagram"/>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuVisible: false,
+    }
+  }
+
+  toggleMenu() {
+    if (this.state.menuVisible) {
+      this.setState({menuVisible: false})
+    } else {
+      this.setState({menuVisible: true})
+    }
+  }
+
+  render() {
+    console.log(this.state.menuVisible);
+    const links = [
+      {
+        name: "sculpture",
+        label: "Sculpture",
+        linkTo: "/",
+      },
+      {
+        name: "about",
+        label: "About",
+        linkTo: "About",
+      },
+      {
+        name: "contact",
+        label: "Contact",
+        linkTo: "Contact",
+      },
+      {
+        name: "videos",
+        label: "Videos",
+        linkTo: "Video",
+      },
+    ]
+
+    // button makes ripple effect
+    return (
+      <div className="menu">
+        <h1 className="nameHeader">Hardy Izard</h1>
+        <div className="sidebar" onClick={() => this.toggleMenu()}>
+          {this.state.menuVisible ?
+            <List disablePadding dense>
+              {links.map(({ label, name, linkTo, ...rest }) => (
+                <ListItem key={name} button {...rest}>
+                <Link to={linkTo}><ListItemText>{label}</ListItemText></Link>
+                </ListItem>
+              ))}
+              <ListItem button>
+              <ListItemText><SiInstagram/></ListItemText>
+            </ListItem>
+            </List>
+            :
+            <FiMenu />
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Menu;
