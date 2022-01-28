99bf481592560f7ede62eb97a1cd94dbc38a3419
Lee Pedersen <leepedersenr@gmail.com>
2022-01-27 20:13:16 -0700

    add more pictures, tweak styling of pictures a bit

--- src/constants/imageList.js
+++ src/constants/imageList.js
@@ -3,10 +3,27 @@ import coyoteskull1 from '../img/coyoteskull1.jpeg';
 import coyoteskull2 from '../img/coyoteskull2.jpeg';
 import coyoteskull3 from '../img/coyoteskull3.jpeg';
 import twoweekspay from '../img/2weekspay.jpeg';
+import bronze01 from '../img/bronze01.jpg';
+import bronze02 from '../img/bronze02.jpg';
+import bronze03 from '../img/bronze03.jpg';
+import bronze04 from '../img/bronze04.jpg';
+import bronze05 from '../img/bronze05.jpg';
+import bronze06 from '../img/bronze06.jpg';
+import bronze07 from '../img/bronze07.jpg';
+import bronze08 from '../img/bronze08.jpg';
+import bronze09 from '../img/bronze09.jpg';
+import bronze10 from '../img/bronze10.jpg';
+import bronze11 from '../img/bronze11.jpg';
+import bronze12 from '../img/bronze12.jpg';
+import bronze13 from '../img/bronze13.jpg';
+import bronze14 from '../img/bronze14.jpg';
+import bronze15 from '../img/bronze15.jpg';
+
+
 
 export const imageList = [
   {
-    src: unsung,
+    src: bronze01,
     alt: "Unsung",
     length: "46",
     width: "26",
@@ -14,34 +31,114 @@ export const imageList = [
     description: "this is a description of the image Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each othe",
   },
   {
-    src: twoweekspay,
+    src: bronze02,
+    alt: "Ride",
+    length: "68",
+    width: "10",
+    depth: "12",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze03,
+    alt: "Veritas",
+    length: "58",
+    width: "19",
+    depth: "14",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze04,
     alt: "Two Weeks Pay",
-    length: "46",
-    width: "26",
+    length: "10",
+    width: "6",
+    depth: "10",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze05,
+    alt: "King of the Wood",
+    length: "23",
+    width: "8",
+    depth: "5",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze06,
+    alt: "Anodyne",
+    length: "44",
+    width: "5",
+    depth: "7",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze07,
+    alt: "Untitled",
+    length: "7",
+    width: "3",
+    depth: "2",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze08,
+    alt: "Cow Elk #1",
+    length: "16",
+    width: "9",
     depth: "6",
     description: "this is a description of the image",
   },
   {
-    src: coyoteskull2,
-    alt: "coyoteskull2",
-    length: "46",
-    width: "26",
+    src: bronze09,
+    alt: "Cow Elk #2",
+    length: "16",
+    width: "9",
     depth: "6",
     description: "this is a description of the image",
   },
   {
-    src: coyoteskull3,
-    alt: "coyoteskull3",
-    length: "46",
-    width: "26",
+    src: bronze10,
+    alt: "Cow Elk #3",
+    length: "16",
+    width: "9",
     depth: "6",
     description: "this is a description of the image",
   },
   {
-    src: coyoteskull1,
-    alt: "coyoteskull1",
-    length: "46",
-    width: "26",
+    src: bronze11,
+    alt: "Coyote #1",
+    length: "23",
+    width: "8",
+    depth: "5",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze12,
+    alt: "Coyote #2",
+    length: "8",
+    width: "4",
+    depth: "3",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze13,
+    alt: "Coyote #3",
+    length: "8",
+    width: "4",
+    depth: "3",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze14,
+    alt: "Coyote #4",
+    length: "8",
+    width: "4",
+    depth: "3",
+    description: "this is a description of the image",
+  },
+  {
+    src: bronze15,
+    alt: "Untitled",
+    length: "8",
+    width: "8",
     depth: "6",
     description: "this is a description of the image",
   },
