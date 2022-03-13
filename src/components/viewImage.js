1c052b17b6f2dd867b612228b503bfbfbc720a34
LeePedersen <leepedersenr@gmail.com>
2022-03-13 16:06:16 -0600

    style viewimage

--- src/components/viewImage.js
+++ src/components/viewImage.js
@@ -13,31 +13,26 @@ class ViewImage extends React.Component {
   }
 
   toggleViewImage(source) {
-    // console.log(this.state);
-    console.log(source); // should be null, hmm
-    // toggle image classname on click
     if (this.state.imgClassName == "invisible") {
-      this.setState({imgClickedSrc: source});
-      this.setState({imgClassName: "imageClicked"});
+      this.setState({imgClassName: "imageClicked", imgClickedSrc: source});
     } else {
-      this.setState({imgClickedSrc: null});
-      this.setState({imgClassName: "invisible"});
+      this.setState({imgClassName: "invisible", imgClickedSrc: null});
     }
   }
 
   render() {
     return (
+
       <div className="imageContainer">
         <div className="column">
-          <div className="image" >
-            <img className="imageInDiv" onClick={() => this.toggleViewImage(this.state.image.src)} src={this.state.image.src} alt={this.state.image.alt}/>
-          </div>
-        {this.state.image.moreImages &&
-          this.state.image.moreImages.map(image => (
-            <img className="image" onClick={() => this.toggleViewImage(image.src)} src={image.src} alt={image.alt}/>
-          ))
-        }
+          <img className="image" onClick={() => this.toggleViewImage(this.state.image.src)} src={this.state.image.src} alt={this.state.image.alt}/>
+          {this.state.image.moreImages &&
+            this.state.image.moreImages.map(image => (
+              <img className="image" onClick={() => this.toggleViewImage(image.src)} src={image.src} alt={image.alt}/>
+            ))
+          }
         </div>
+
         <div className="column2">
           <h1>{this.state.image.alt}</h1>
           <p>L = {this.state.image.length}"</p>
@@ -45,8 +40,9 @@ class ViewImage extends React.Component {
           <p>D = {this.state.image.depth}"</p>
           <p className="description">{this.state.image.description}</p>
         </div>
-        <div className={this.state.imgClassName} >
-            <img className="imageInDiv" onClick={() => this.toggleViewImage()} src={this.state.imgClickedSrc} alt={this.state.image.alt}/>
+
+        <div className={this.state.imgClassName} onClick={() => this.toggleViewImage()}>
+            <img className="image" src={this.state.imgClickedSrc} alt={this.state.image.alt}/>
         </div>
 
       </div>
