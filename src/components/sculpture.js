213004506703679ac7263d5c544e0a7dddfbc90a
LeePedersen <leepedersenr@gmail.com>
2022-05-05 15:24:26 -0600

    tweak some styling, get rid of unique key warning

--- src/components/sculpture.js
+++ src/components/sculpture.js
@@ -6,9 +6,7 @@ import { imageList } from '../constants/imageList';
 function Sculpture() {
   const halfway = Math.ceil(imageList.length / 2);
   const column1list = imageList.slice(0, halfway);
-  console.log(column1list.length);
   const column2list = imageList.slice(halfway);
-  console.log(column2list.length);
 
   return (
     <>
@@ -16,18 +14,18 @@ function Sculpture() {
       <div className="Sculpture">
         <div className="sColumn">
           {column1list.map((image, index) =>
-            <>
-              <Link key={index} to={{ pathname: "viewImage", state: { staticContext: column1list[index] }}}><img className="sculptureImage" src={column1list[index].src} alt={column1list[index].alt}/></Link>
+            <div key={index}>
+              <Link to={{ pathname: "viewImage", state: { staticContext: column1list[index] }}}><img className="sculptureImage" src={column1list[index].src} alt={column1list[index].alt}/></Link>
               <p className="description">{image.alt} <br/> l = {image.length}" <br/> w = {image.width}" <br/> d = {image.depth}"</p>
-            </>
+            </div>
           )}
         </div>
         <div className="sColumn">
           {column2list.map((image, index) =>
-            <>
-              <Link key={index} to={{ pathname: "viewImage", state: { staticContext: column2list[index] }}}><img className="sculptureImage" src={column2list[index].src} alt={column2list[index].alt}/></Link>
+            <div key={(index + column1list.length)}>
+              <Link to={{ pathname: "viewImage", state: { staticContext: column2list[index] }}}><img className="sculptureImage" src={column2list[index].src} alt={column2list[index].alt}/></Link>
               <p className="description">{image.alt} <br/> l = {image.length}" <br/> w = {image.width}" <br/> d = {image.depth}"</p>
-            </>
+            </div>
           )}
           
         </div>
